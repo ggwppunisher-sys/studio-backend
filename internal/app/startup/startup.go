db95d28c80fd7372a0e4594629ac3d869993995d
Viachaslau Timertsau <timercev.v@rwb.ru>
2025-12-21 20:59:23 +0300

    feat: init startup

--- internal/app/startup/startup.go
+++ internal/app/startup/startup.go
@@ -2,10 +2,36 @@ package startup
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
 	"studio-backend/internal/app/config"
-	"studio-backend/internal/domain"
+	"studio-backend/internal/transport/apiserver"
 )
 
-func Run(_ context.Context, _ config.Config) error {
-	return domain.ErrNotImplemented
+func Run(ctx context.Context, cfg config.Config) error {
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusTeapot)
+	})
+	server, err := apiserver.NewServer(handler, cfg.Server)
+	if err != nil {
+		return fmt.Errorf("failed to init server: %w", err)
+	}
+
+	go func() {
+		<-ctx.Done()
+		err := server.Shutdown(ctx)
+		if err != nil {
+			slog.Error("failed to shutdown", "error", err)
+		}
+	}()
+
+	slog.Info("Server started", "port", cfg.Server.Port)
+	err = server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		return nil
+	}
+	return err
 }
