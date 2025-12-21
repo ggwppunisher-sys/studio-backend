780ee533b670eb0c415d095fcd2dbdc3a706a589
Viachaslau Timertsau <timercev.v@rwb.ru>
2025-12-21 20:58:57 +0300

    feat: add server config

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,11 +1,28 @@
 package config
 
 import (
-	"studio-backend/internal/domain"
+	"time"
+
+	"github.com/caarlos0/env/v11"
 )
 
-type Config struct{}
+type Config struct {
+	Server  ServerConfig
+	AppName string
+	Version string
+}
 
-func New(_, _ string) (Config, error) {
-	return Config{}, domain.ErrNotImplemented
+func New(appName, version string) (Config, error) {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return Config{}, err
+	}
+	cfg.AppName = appName
+	cfg.Version = version
+	return cfg, nil
+}
+
+type ServerConfig struct {
+	Port         string        `env:"HTTP_PORT"`
+	WriteTimeout time.Duration `env:"HTTP_WRITE_TIMEOUT"`
 }
