e9d27ac0a6d4f600a6ac15be19541f279e5f16b5
slavatimr <timercev.v@rwb.ru>
2026-01-01 19:41:30 +0300

    feat: satisfy strict server interface

--- internal/transport/apiserver/handlers/api.go
+++ internal/transport/apiserver/handlers/api.go
@@ -1,10 +1,34 @@
 package handlers
 
-import "studio-backend/internal/domain"
+import (
+	"context"
+	"studio-backend/internal/domain"
+	"studio-backend/internal/transport/apiserver/gen"
+)
 
 type StrictImplementation struct{}
 
 func NewStrictImplementation() *StrictImplementation {
-	panic(domain.ErrNotImplemented)
 	return &StrictImplementation{}
+}
+
+func (si *StrictImplementation) V1CreateUser(
+	_ context.Context,
+	_ gen.V1CreateUserRequestObject,
+) (gen.V1CreateUserResponseObject, error) {
+	return nil, domain.ErrNotImplemented
+}
+
+func (si *StrictImplementation) V1GetUser(
+	_ context.Context,
+	_ gen.V1GetUserRequestObject,
+) (gen.V1GetUserResponseObject, error) {
+	return nil, domain.ErrNotImplemented
+}
+
+func (si *StrictImplementation) V1UpdateUser(
+	_ context.Context,
+	_ gen.V1UpdateUserRequestObject,
+) (gen.V1UpdateUserResponseObject, error) {
+	return nil, domain.ErrNotImplemented
 }
