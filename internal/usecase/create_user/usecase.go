c34350ca0fae1021bc010a89d315ebbb6584fd57
Makar <ggwppunisher@gmail.com>
2026-01-04 10:23:55 +0300

    add: new usecase for save users

--- internal/usecase/create_user/usecase.go
+++ internal/usecase/create_user/usecase.go
@@ -1,13 +1,33 @@
 package createUser
 
-import "studio-backend/internal/domain"
+import (
+	"context"
+	"studio-backend/internal/domain"
 
-type UseCase struct{}
+	"github.com/google/uuid"
+)
 
-func New() *UseCase {
-	return &UseCase{}
+type UserSaver interface {
+	SaveUser(ctx context.Context, user domain.User) (int64, error)
+}
+type UseCase struct {
+	saver UserSaver
 }
 
-func (uc *UseCase) Create() error {
-	return domain.ErrNotImplemented
+func New(saver UserSaver) *UseCase {
+	return &UseCase{
+		saver: saver,
+	}
+}
+
+func (uc *UseCase) Create(ctx context.Context, user domain.User) error {
+	user.Id = uuid.New()
+	if err := user.Validate(); err != nil {
+		return err
+	}
+	_, err := uc.saver.SaveUser(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
 }
