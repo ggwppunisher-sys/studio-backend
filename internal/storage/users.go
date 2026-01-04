efa840347936b6dd3b50e1a61a39e6fb3ac7f252
Makar <ggwppunisher@gmail.com>
2026-01-04 10:22:48 +0300

    add: saveusers in db

--- internal/storage/users.go
+++ internal/storage/users.go
@@ -1,17 +1,41 @@
 package storage
 
 import (
+	"context"
 	"database/sql"
 	"studio-backend/internal/domain"
 )
 
 const (
-	createUserQ = ``
+	createUserQ = `
+	INSERT INTO um.users (id, first_name, last_name, username, tg_id, tg_chat_id)
+	VALUES (1$, 2$, 3$, 4$, 5$, 6$)
+	RETURNING tg_id
+	`
 	updateUserQ = ``
 )
 
-type UsersStorage struct{}
+type UsersStorage struct {
+	db *sql.DB
+}
 
 func NewUsersStorage(db *sql.DB) (*UsersStorage, error) {
-	return nil, domain.ErrNotImplemented
+	return &UsersStorage{db: db}, nil
+}
+
+func (s *UsersStorage) SaveUser(ctx context.Context, user domain.User) (int64, error) {
+	var id int64
+	err := s.db.QueryRowContext(ctx, createUserQ,
+		user.Id,                   //$1
+		user.TgUserInfo.FirstName, // $2
+		user.TgUserInfo.LastName,  // $3
+		user.TgUserInfo.Username,  // $4
+		user.TgUserInfo.TgId,      // $5
+		user.TgUserInfo.TgChatId,  // $6
+	).Scan(&id)
+
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
